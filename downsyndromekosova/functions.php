6820b2c9757c741562b388425cdde4184d496bbd
Dielli Demjaha <diellidemjaha@gmail.com>
2024-04-02 08:49:22 +0200

    Update functions.php

--- downsyndromekosova/functions.php
+++ downsyndromekosova/functions.php
@@ -1,35 +1,22 @@
 <?php
 require_once get_stylesheet_directory() . '/custom-walker.php';
-// Enqueue styles and scripts
 function my_custom_theme_enqueue_styles() {
-    // Enqueue the parent theme stylesheet (if your theme is a child theme)
-    // Example: wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
-    // Enqueue your theme's stylesheet
     wp_enqueue_style('style', get_stylesheet_uri());
 
-    // Enqueue jQuery (uncomment the line below if your theme requires jQuery)
-    // wp_enqueue_script('jquery');
 
-    // Enqueue your custom JavaScript file
-    // wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/custom.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'my_custom_theme_enqueue_styles');
 
-// Register a custom navigation menu
 function my_custom_theme_register_menus() {
     register_nav_menu('primary', __('Primary Menu', 'my-custom-theme'));
 }
 add_action('after_setup_theme', 'my_custom_theme_register_menus');
 
-// Add theme support for custom logos
 function my_custom_theme_setup() {
-    // Add support for custom logos (uncomment the line below)
-    // add_theme_support('custom-logo');
 }
 add_action('after_setup_theme', 'my_custom_theme_setup');
 
-// Add theme support for featured images (post thumbnails)
 function my_custom_theme_add_featured_image_support() {
     add_theme_support('post-thumbnails');
 }
@@ -42,12 +29,11 @@ function enqueue_bootstrap() {
 add_action('wp_enqueue_scripts', 'enqueue_bootstrap');
 
 function custom_excerpt_length($length) {
-    return 12; // Change 30 to the desired number of characters
+    return 12; 
 }
 add_filter('excerpt_length', 'custom_excerpt_length');
 
 
-// Register a custom post type for image gallery
 function custom_image_gallery_post_type() {
     $labels = array(
         'name'               => 'Slider Gallery',
@@ -76,10 +62,8 @@ function custom_image_gallery_post_type() {
 }
 add_action('init', 'custom_image_gallery_post_type');
 
-// Enable featured image for custom post type
 add_theme_support('post-thumbnails');
 
-// Modify the query to show custom post type on the home page
 function modify_homepage_query($query) {
     if ($query->is_home() && $query->is_main_query()) {
         $query->set('post_type', array('post', 'slider_gallery'));
@@ -108,7 +92,7 @@ function custom_text_content_post_type() {
         'public'              => true,
         'menu_position'       => 6,
         'menu_icon'           => 'dashicons-text',
-        'supports'            => array('title', 'editor'), // Add 'editor' for text content
+        'supports'            => array('title', 'editor'), 
         'rewrite'             => array('slug' => 'text-content'),
     );
 
@@ -116,8 +100,6 @@ function custom_text_content_post_type() {
 }
 add_action('init', 'custom_text_content_post_type');
 
-// Enable featured image support for pages
-// Add custom thumbnail field support to the "page" post type
 function add_custom_thumbnail_field_support() {
     add_post_type_support('page', 'custom-fields');
 }
@@ -130,9 +112,9 @@ function create_staff_post_type() {
             'singular_name' => 'Staff Member',
         ),
         'public' => true,
-        'supports' => array('thumbnail', 'editor'), // Remove 'title' from supports
-        'show_in_menu' => true, // Display in the admin menu
-        'menu_position' => 5, // Adjust the menu position as needed
+        'supports' => array('thumbnail', 'editor'), 
+        'show_in_menu' => true, 
+        'menu_position' => 5, 
     ));
 }
 add_action('init', 'create_staff_post_type');
